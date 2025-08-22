9b2943469149b921e26422e2b8c7f490bfbb666b
iJaabl <jaabel@pm.me>
2025-08-21 23:23:19 -0400

    config css

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
             {/* product details */}
             <div>
               <p>
-                <span></span>
+                <span>Product details</span>
               </p>
             </div>
           </div>
